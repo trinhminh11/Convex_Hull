aa8c64af0c4ee3944aceea86c4430a18c3b54a0b
trinhminh11 <72061760+trinhminh11@users.noreply.github.com>
2021-07-12 10:41:10 +0700

    Update main.js

--- fireworks/main.js
+++ fireworks/main.js
@@ -2,17 +2,21 @@ var fireworks = []
 var gravity
 
 function setup(){
-	createCanvas(800, 500)
+	createCanvas(window.innerWidth, window.innerHeight)
 	gravity = createVector(0, .2)
 	stroke(255)
 	strokeWeight(4)
 	fireworks.push(new Firework())
 	background(0, 0, 0)
+	textSize(64)
 }
 
 function draw(){
 	colorMode(RGB)
+	fill('red')
 	background(0, 0, 0, 25)
+	t = 'Happy Birthday :3'
+	text(t, width/2-textWidth(t)/2, 100)
 
 	if (random(1) < .03 || fireworks.length < 2) {
 		fireworks.push(new Firework())
